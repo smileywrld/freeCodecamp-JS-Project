a5b8b82bfc37c79fd6f5b074954fead79c710473
smileywrld <ismaileyyunusa@gmail.com>
2026-01-07 17:10:18 +0100

    Add triviaBot script to share coding fun facts

---
+++ variables & Strings/triviaBot.js
@@ -0,0 +1,23 @@
+console.log("Hello!, I'm your coding fun fact guide!");
+
+const botName = "SmyLey";
+const botLocation = "Lagos";
+const favoriteLanguage = "JavaScript";
+
+console.log("My name is " + botName + " and I live on " + botLocation);
+
+console.log("My favorite programming language os " + favoriteLanguage);
+
+let codingFact = "Do you know that " + favoriteLanguage + " is one of the top languages in the world?";
+
+console.log(codingFact);
+
+codingFact = favoriteLanguage + " Is also very easy to learn!";
+
+console.log(codingFact);
+
+codingFact = "Research also state that " + favoriteLanguage + " some people find it difficult, and i might be one of them";
+
+console.log(codingFact);
+
+console.log("It was fun sharing these facts with you. Goodbye! - " + botName +" from " + botLocation + ".");
