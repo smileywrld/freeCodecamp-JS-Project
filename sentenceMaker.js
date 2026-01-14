8d1d0bad3a18df3439c5a23720f7901112ae9c2e
smileywrld <ismaileyyunusa@gmail.com>
2026-01-14 21:51:07 +0100

    Add sentenceMaker script to generate whimsical stories

---
+++ sentenceMaker.js
@@ -0,0 +1,22 @@
+let adjective = "Happy";
+let noun = "Ismail";
+let verb = "Fight";
+let place = "Abuja";
+let adjective2 = "smile";
+let noun2 = 'Jollof Rice';
+
+const firstStory = `Once upon a time, there was a(n) ${adjective} ${noun} who loved to eat ${noun2}. The ${noun} lived in a ${place} and had ${adjective2} nostrils that blew fire when it was ${verb}.`;
+
+console.log(`First story: ${firstStory}`);
+
+adjective = "Sad";
+noun = "boy";
+verb = "dance";
+place = "Lagos";
+adjective2 = 'laugh';
+noun2 = 'sam';
+
+const secondStory = `Once upon a time, there was a(n) ${adjective} ${noun} who loved to eat ${noun2}. The ${noun} lived in a ${place} and had ${adjective2} nostrils that blew fire when it was ${verb}.`
+
+console.log(`Second story: ${secondStory}`);
+
